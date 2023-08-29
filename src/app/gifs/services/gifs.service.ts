8bb86864392f0b8ba650ba1b7fe304c98592bddc
Carlos Lafont <carloslafont1997@gmail.com>
2023-08-28 23:28:35 -0500

    Gif App

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,69 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { SearchResponse, Gif } from '../interfaces/gifs.interfaces';
+
+@Injectable({providedIn: 'root'})
+export class GifsService {
+   
+    public gifsList: Gif[] = [];
+    private _tagsHistory: string[]=[];
+    private apiKey:       string = 'wLeX434btOYaeAQRZWrmZTwM7Zd4Tety';
+    private serviceUrl:   string = 'https://api.giphy.com/v1/gifs';
+   
+    constructor( private http: HttpClient ) { 
+        this.loadLocalStorage();
+        if(this._tagsHistory.length == 0)return;
+        this.searchTag(this._tagsHistory[0]);
+     }
+
+    get tagsHistory(){
+        return [...this._tagsHistory];
+    }
+
+    private organizeHistory( tag:string ){
+
+        tag = tag.toLowerCase();
+
+        if( this._tagsHistory.includes(tag) ){
+            this._tagsHistory = this._tagsHistory.filter( (oldTag) => oldTag !== tag);
+        }
+
+        this._tagsHistory.unshift( tag );
+        this._tagsHistory = this._tagsHistory.splice(0,10);
+        this.saveLocalStorage();
+    }
+
+    private saveLocalStorage():void{
+        localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+    }
+
+    private loadLocalStorage():void{
+        if(!localStorage.getItem('history'))return;
+
+        this._tagsHistory = JSON.parse(localStorage.getItem('history')! );
+        
+    }
+    
+    /*async*/ searchTag( tag: string ): /*Promise<void>*/ void {
+        
+        if(tag.length === 0) return;
+        this.organizeHistory( tag );
+        // fetch('https://api.giphy.com/v1/gifs/search?q=valoran&limit=10&api_key=wLeX434btOYaeAQRZWrmZTwM7Zd4Tety')
+        // .then(resp => resp.json())
+        // .then(data => console.log(data));
+
+        const params = new HttpParams()
+            .set('api_key', this.apiKey )
+            .set('limit','10' )
+            .set('q', tag );
+
+        this.http.get<SearchResponse>(`${this.serviceUrl}/search`,{ params } )
+            .subscribe( resp => {
+
+                this.gifsList = resp.data;
+                console.log( {gifs: this.gifsList} );
+            })
+    }
+
+}
